ca01fe56099082df86433f066a892028e0c459a8
Rahul Moglappa Jog <121598150+rahuljogrj@users.noreply.github.com>
2023-07-27 12:32:12 +0530

    Update Multidimensional_array.java

--- Thursday_Lab_13th_july/src/Thursday_Lab_27th_july/Multidimensional_array.java
+++ Thursday_Lab_13th_july/src/Thursday_Lab_27th_july/Multidimensional_array.java
@@ -20,16 +20,14 @@ public class Multidimensional_array {
 		for(i=0; i<2; i++) {
 			for(j=0; j<3; j++) {
 				arr[i][j] = sc.nextInt();
-				}
+			}
 		}
-		
-		
 		
 
 		for(i=0; i<2; i++) {
 			for(j=0; j<3; j++) {
 				System.out.print(j+"\t");
-				}
+			}
 			System.out.println(i);
 		}
 		
