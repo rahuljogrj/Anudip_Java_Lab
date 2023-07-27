eb8c0f058106128ca64c07c3262df20e915f5da2
Rahul Moglappa Jog <121598150+rahuljogrj@users.noreply.github.com>
2023-07-27 17:15:59 +0530

    Update Array_program.java

--- Thursday_Lab_13th_july/src/Thursday_Lab_27th_july/Array_program.java
+++ Thursday_Lab_13th_july/src/Thursday_Lab_27th_july/Array_program.java
@@ -5,11 +5,16 @@ public class Array_program extends Var_Scan {
 	
 	
 	
+	
+	public void array() {
+		arr=accept();
+	}
+	
+	
+		
 //find greater number from array
 	public void grt() {
-		System.out.println("\n----- find Greatest number from Array -----");
-		
-		int[] arr=accept();
+		array();
 		int grt = arr[0];
 		for(int i=0; i<arr.length; i++) {
 			if(grt<arr[i]) {
@@ -17,7 +22,7 @@ public class Array_program extends Var_Scan {
 			}
 		}
 		
-		System.out.println("\nthe greatest number in array is: "+grt);
+		System.out.println("\nThe greatest number in array is: "+grt+"\n");
 		
 	}
 	
@@ -25,8 +30,8 @@ public class Array_program extends Var_Scan {
 	
 //	find smallest value from array
 	public void smt() {
-		System.out.println("\n----- find Smallest number from Array -----");
-		int[] arr=accept();
+//		System.out.println("\n----- find Smallest number from Array -----");
+//		int[] arr=accept();
 		smt = arr[0];
 		for(i=0; i<arr.length; i++) {
 			if(smt>arr[i]) {
@@ -34,7 +39,7 @@ public class Array_program extends Var_Scan {
 			}
 		}
 		
-		System.out.println("\nThe smallest number from array is: "+smt);
+		System.out.println("\nThe smallest number from array is: "+smt+"\n");
 			
 	}
 	
@@ -42,8 +47,8 @@ public class Array_program extends Var_Scan {
 	
 //	print ascending order or array
 	public void ascen() {
-		System.out.println("\n\n----- Sort array by Ascending order-----");
-		int[] arr = accept();
+//		System.out.println("\n\n----- Sort array by Ascending order-----");
+//		int[] arr = accept();
 		temp = 0;
 		for(i=0; i<size; i++) {
 			for(j=i+1; j<size; j++) {
@@ -61,13 +66,12 @@ public class Array_program extends Var_Scan {
 		Print_array();
 	}
 	
-	
-	
+
 	
 //	print descending order or array
 	public void descen() {
-		System.out.println("\n\n----- Sort array by Descending order -----\n");
-		int[] arr = accept();
+//		System.out.println("\n\n----- Sort array by Descending order -----\n");
+//		int[] arr = accept();
 		temp = 0;
 		for(i=0; i<arr.length; i++) {
 			for(j=i+1; j<arr.length; j++) {
@@ -85,5 +89,21 @@ public class Array_program extends Var_Scan {
 	}
 	
 
+	// Print Array one by one
+	public void Print_array() {
+		for(i=0; i<arr.length; i++) {
+			System.out.print("\t"+arr[i]);
+		}
+		System.out.println("\n");
+	}
+	
+	
+	
+	// condition to find ascending and descending order of array
+		public void Array_ascen_descen() {
+			temp = arr[i];
+			arr[i]=arr[j];
+			arr[j] = temp;
+		}
 
 }
