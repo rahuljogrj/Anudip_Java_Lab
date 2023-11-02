993548081545ab6dc24765cf3c5ebed06ce7778a
Rahul Moglappa Jog <121598150+rahuljogrj@users.noreply.github.com>
2023-11-02 14:12:14 +0530

    Delete BookManagementSystem directory

--- BookManagementSystem/src/main/java/com/bookManageSystem/repository/Book_repository.java
+++
@@ -1,9 +0,0 @@
-package com.bookManageSystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bookManageSystem.entity.Book_entity;
-
-public interface Book_repository extends JpaRepository<Book_entity, Integer> {
-
-}
